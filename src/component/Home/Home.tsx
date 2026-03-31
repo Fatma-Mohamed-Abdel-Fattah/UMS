fd82e59965182f15709bedc35178dee115b100ad
Fatma-Mohamed-Abdel-Fattah <Fatma-Mohamed-Abdel-Fattah>
2026-04-01 01:33:47 +0200

    home

--- src/component/Home/Home.tsx
+++ src/component/Home/Home.tsx
@@ -1,7 +1,39 @@
-// import React from 'react'
+import styles from "./Home.module.css";
+import { useContext } from "react";
+import { BarChart, Bar, XAxis, YAxis, Tooltip, ResponsiveContainer } from "recharts";
+import { AuthContext } from "../context/AuthContext";
+interface User{
+  firstName:string;
+  lastName:string;
+}
+interface AuthContextType{
+  userData:User | null
 
+}
 export default function Home() {
+  let {userData}=useContext(AuthContext) as AuthContextType
+  const data = [
+    { name: "Users", value: 120 },
+    { name: "Admins", value: 10 },
+    { name: "Active", value: 80 },
+  ];
   return (
-    <div>Home</div>
+    <>
+      <div className="container mx-3 mb-4 ">
+      <h3 className={`${styles.welcome} my-4`}>Welcome Back {userData?.firstName} {userData?.lastName}</h3>
+
+      <div style={{ width: "90%", height: 300 }}>
+        <ResponsiveContainer>
+          <BarChart data={data} className={styles.barchart}>
+            <XAxis dataKey="name" />
+            <YAxis />
+            <Tooltip />
+            <Bar dataKey="value" fill="#feaf00" />
+          </BarChart>
+        </ResponsiveContainer>
+      </div>
+
+    </div>
+    </>
   )
 }
--- src/component/shared/NavBar/NavBar.tsx
+++ src/component/shared/NavBar/NavBar.tsx
@@ -1,7 +1,25 @@
 // import React from 'react'
-
+import { IoIosNotificationsOutline } from "react-icons/io";
+import styles from "./NavBar.module.css";
 export default function NavBar() {
-  return (
-    <div>NavBar</div>
-  )
+   return (
+      <>
+         <div>
+            <nav className="navbar bg-body-tertiary">
+               <div className="container-fluid">
+                  <h1 >
+                     <a className={`navbar-brand ${styles.logo} `}>UMS</a>
+                  </h1>
+                  <form className="d-flex" role="search">
+                     <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
+                     <button className="btn btn-outline-warning" type="submit">
+                        Search
+                     </button>
+                     <IoIosNotificationsOutline size={50} />
+                  </form>
+               </div>
+            </nav>
+         </div>
+      </>
+   );
 }
